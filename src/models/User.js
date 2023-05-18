eaecbc566e1ef95a1bea884715c1d338ab8d8afd
Saad Farooq <saadfarooq.info@gmail.com>
2023-05-18 17:58:09 +0500

    node-session-5

--- src/models/User.js
+++ src/models/User.js
@@ -1,3 +1,4 @@
+import bcrypt from 'bcrypt'
 import mongoose from "mongoose"
 
 const { Schema } = mongoose
@@ -12,10 +13,29 @@ const userSchema = new Schema(
       type: String,
       required: true,
       unique: true
+    },
+    password: {
+      type: String,
+      max: 255,
+      required: true
     }
   },
   { timestamps: true }
 )
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified("password")) return next();
+
+  const saltRound = 10
+  const password = this.password
+
+  const salt = await bcrypt.genSalt(saltRound)
+  const hash = await bcrypt.hash(password, salt)
+
+  this.password = hash
+  next()
+})
+
 
 const userModel = mongoose.model('User', userSchema)
 
